01c509b13d51b8396e8d2908f43206fda59bbf4b
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-06-01 21:22:26 -0700

    added header

--- components/Header.js
+++ components/Header.js
@@ -7,20 +7,20 @@ HomeIcon,
 UserGroupIcon,
 ViewGridIcon,
 } from "@heroicons/react/solid";
- 
+
 import {
 FlagIcon,
 PlayIcon,
 SearchIcon,
 ShoppingCartIcon,
 } from "@heroicons/react/outline";
-import HeaderIcon from "./HeaderIcon"
+import HeaderIcon from "./HeaderIcon";
 // ==================================================================================//
 // ==================================================================================//
 
 function Header() {
 return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md ">
     {/* ========================================== */}
     {/* Left */}
     <div className="flex items-center">
@@ -33,7 +33,7 @@ return (
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
         <SearchIcon className="h-6 text-gray-600" />
         <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            className="hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
         />
@@ -43,16 +43,25 @@ return (
     {/* Center */}
     <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md:space-x-2">
-            <HeaderIcon Icon={HomeIcon}/>
-            <HeaderIcon Icon={FlagIcon}/>
-            <HeaderIcon Icon={PlayIcon}/>
-            <HeaderIcon Icon={ShoppingCartIcon}/>
-            <HeaderIcon Icon={UserGroupIcon}/>
+        <HeaderIcon active Icon={HomeIcon} />
+        <HeaderIcon Icon={FlagIcon} />
+        <HeaderIcon Icon={PlayIcon} />
+        <HeaderIcon Icon={ShoppingCartIcon} />
+        <HeaderIcon Icon={UserGroupIcon} />
         </div>
     </div>
 
     {/* ========================================== */}
     {/* Right */}
+    <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile Pic */}
+        <p className='whitespace-nowrap font semi-bold pr-3'>Daniel James</p>
+        <ViewGridIcon className='icon'/>
+        <ChatIcon className='icon'/>
+        <BellIcon className='icon'/>
+        <ChevronDownIcon className='icon'/>
+
+    </div>
 
     {/* ========================================== */}
     </div>
